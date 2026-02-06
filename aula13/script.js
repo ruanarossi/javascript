a41dce571190b6ac016d10f73434f447e6ac312e
Ruana Vieira <ruana-rossi@hotmail.com>
2026-02-06 16:09:58 -0300

    Atualização

---
+++ aula13/script.js
@@ -0,0 +1,26 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+} while (c <= 6)
+
+
+
+/*   tanto faz essa maneira ou a de cima! (mais usada)
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+}
+*/
+
+
+
+/* pior solução em repetição 
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
